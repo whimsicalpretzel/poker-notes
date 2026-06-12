794b27a4472889dd25df44d81238d4903ce418fb
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2026-06-12 16:03:52 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,6 +0,0 @@
-// Replace these placeholder values with the configuration shown in:
-// Firebase Console > Project settings > Your apps > SDK setup and configuration
-//
-// This configuration identifies your Firebase project. It is normal for these
-// values to be present in frontend code. Firestore Security Rules protect data.
-
