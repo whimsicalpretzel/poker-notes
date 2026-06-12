fcad3561eec5be6cafb694062a907c3dad71af17
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2026-06-12 16:01:27 -0400

    Remove Firebase configuration
    Removed Firebase configuration object from the file.

--- firebase-config.js
+++ firebase-config.js
@@ -4,11 +4,3 @@
 // This configuration identifies your Firebase project. It is normal for these
 // values to be present in frontend code. Firestore Security Rules protect data.
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA5wd3u91fIyrmwoKv4W9GPnxzi_9ztWkQ",
-    authDomain: "poker-notes-363ed.firebaseapp.com",
-    projectId: "poker-notes-363ed",
-    storageBucket: "poker-notes-363ed.firebasestorage.app",
-    messagingSenderId: "235418951703",
-    appId: "1:235418951703:web:4856824af25c589289bc52"
-  };
